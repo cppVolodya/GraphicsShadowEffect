77a538a80560712a38fd3cbd808430ccb026e5cc
Volodymyr Dorozhovets <dorozovecvoloda@gmail.com>
2022-12-16 14:16:33 +0200

    added types_aliases

---
+++ headers/types_aliases.hpp
@@ -0,0 +1,15 @@
+// Copyright [2022] <Volodymyr Dorozhovets>"
+
+#ifndef TYPES_ALIASES_HPP
+#define TYPES_ALIASES_HPP
+
+#include <QtGlobal>
+
+
+namespace N_GraphicsShadowEffect::N_TypesAliases
+{
+using T_BlurRadius = qreal;
+using T_Thickness  = qreal;
+}  // namespace N_GraphicsShadowEffect::N_TypesAliases
+
+#endif  // TYPES_ALIASES_HPP
