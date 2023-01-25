956a82373b2932fc7585c71a1e6cfcf382d409bd
Volodymyr Dorozhovets <dorozovecvoloda@gmail.com>
2023-01-25 15:09:37 +0200

    added basic implementation of MainWindow class

---
+++ sources/main_window.cpp
@@ -0,0 +1,43 @@
+// Copyright [2022] <Volodymyr Dorozhovets>"
+
+#include <QMouseEvent>
+#include <QColorDialog>
+
+#include "main_window.hpp"
+#include "default_values.hpp"
+
+
+namespace N_GraphicsShadowEffect
+{
+MainWindow::MainWindow(QWidget *parent)
+	: QWidget(parent, Qt::Window),
+	  m_graphics_shadow_effect_for_window(std::make_unique<GraphicsShadowEffect>(this).release()),
+      m_graphics_shadow_effect_for_widget(std::make_unique<GraphicsShadowEffect>(this).release()),
+
+	  m_button_widget(nullptr),
+	  m_exit_button  (nullptr),
+
+	  m_slider_of_blur_radius	     (nullptr),
+	  m_slider_of_thickness  		 (nullptr),
+	  m_slider_of_alpha_color_channel(nullptr),
+
+	  m_label_of_blur_radius		(nullptr),
+	  m_label_of_thickness  		(nullptr),
+	  m_label_of_alpha_color_channel(nullptr),
+
+	  m_check_box_of_window(nullptr),
+	  m_check_box_of_widget(nullptr),
+
+	  m_state_of_check_box_of_window(0),
+	  m_state_of_check_box_of_widget(0),
+
+	  m_mouse_is_pressed(false)
+{
+	this->SetDefaultSettings			();
+	this->CustomizeGraphicsShadowEffects();
+	this->CustomizeWidgets				();
+	this->ConfigureBoxLayout			();
+	this->SetGraphicsShadowEffects  	();
+	this->ConnectSignalsToSlots			();
+}
+}  // namespace N_GraphicsShadowEffect
