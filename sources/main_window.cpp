060a5980b4b5ec343f56a42a2629a02a701a96e3
Volodymyr Dorozhovets <dorozovecvoloda@gmail.com>
2023-01-30 18:27:50 +0200

    added the implementation of the CustomizeWidgets method of the MainWindow class

--- sources/main_window.cpp
+++ sources/main_window.cpp
@@ -68,4 +68,60 @@ void MainWindow::CustomizeGraphicsShadowEffects(GraphicsShadowEffect *graphics_s
 	graphics_shadow_effect->SetThickness (N_DefaultValues::S_THICKNESS  );
 	graphics_shadow_effect->SetColor	  (N_DefaultValues::S_COLOR		 );
 }
+
+inline void MainWindow::CustomizeWidgets()
+{
+	this->CustomizeButtonWidgets  ();
+	this->CustomizeCheckBoxWidgets();
+	this->CustomizeSliderWidgets  ();
+	this->CustomizeLabelWidgets	  ();
+}
+
+inline void MainWindow::CustomizeButtonWidgets()
+{
+	this->m_button_widget = std::make_unique<QPushButton>("Change Color", this).release();
+	this->m_exit_button   = std::make_unique<QPushButton>("Exit", 		  this).release();
+}
+
+void MainWindow::CustomizeCheckBoxWidgets()
+{
+	this->m_check_box_of_window = std::make_unique<QCheckBox>("Window", this).release();
+	this->m_check_box_of_window->setChecked(false);
+	this->m_check_box_of_widget = std::make_unique<QCheckBox>("Widget", this).release();
+	this->m_check_box_of_widget->setChecked(false);
+}
+
+void MainWindow::CustomizeSliderWidgets()
+{
+	this->CustomizeSliderWidgetsOfBlurRadius	   ();
+	this->CustomizeSliderWidgetsOfThickness 	   ();
+	this->CustomizeSliderWidgetsOfAlphaColorChannel();
+}
+
+inline void MainWindow::CustomizeSliderWidgetsOfBlurRadius()
+{
+	this->m_slider_of_blur_radius = std::make_unique<QSlider>(Qt::Horizontal, this).release();
+	this->m_slider_of_blur_radius->setRange(N_DefaultValues::S_RANGE_FOR_SLIDER_MIN,
+											N_DefaultValues::S_RANGE_FOR_SLIDER_OF_BLUR_RADIUS_MAX);
+}
+
+inline void MainWindow::CustomizeSliderWidgetsOfThickness()
+{
+	this->m_slider_of_thickness = std::make_unique<QSlider>(Qt::Horizontal, this).release();
+	this->m_slider_of_thickness->setRange(N_DefaultValues::S_RANGE_FOR_SLIDER_MIN,
+										  N_DefaultValues::S_RANGE_FOR_SLIDER_OF_THICKNESS_MAX);
+}
+
+inline void MainWindow::CustomizeSliderWidgetsOfAlphaColorChannel()
+{
+	this->m_slider_of_alpha_color_channel = std::make_unique<QSlider>(Qt::Horizontal, this).release();
+	this->m_slider_of_alpha_color_channel->setRange(N_DefaultValues::S_RANGE_FOR_SLIDER_MIN, N_DefaultValues::S_RANGE_FOR_SLIDER_OF_COLOR_MAX);
+}
+
+inline void MainWindow::CustomizeLabelWidgets()
+{
+	this->m_label_of_blur_radius = std::make_unique<QLabel>		   ("Blur Radius        ", this).release();
+	this->m_label_of_thickness = std::make_unique<QLabel>  		   ("Thickness          ", this).release();
+	this->m_label_of_alpha_color_channel = std::make_unique<QLabel>("Alpha Color Channel", this).release();
+}
 }  // namespace N_GraphicsShadowEffect
